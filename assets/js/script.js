df188134e8dbeded6e307fea11b3ffef7060ef86
CraigGalorDev <craigmitchellasus@gmail.com>
2022-06-04 21:56:31 +0000

    fixed score counter and started game flow corrections

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,17 +30,14 @@ function clear(){
  * computerSelect string value of "rock" etc will equal the current numerical
  * value set for that hand for this iteration.
  */
-let playerSelect=0,computerSelect=0;
+let playerSelect = 0,computerSelect = 0;
 /**
  * The win and lost counters are set each iteration(Start button being clicked)
  * as stated above the winners hand has a greater numerical value than the loser
  * this only counts the wins and losses
  */
-let winCount=0,lostCount=0;
-let wcount = document.getElementById('winCount');
-wcount.toString(winCount);
-let lcount = document.getElementById('lostCount');
-lcount.toString(lostCount);
+let winCount = 0, lostCount = 0;
+
 
 
 /**
@@ -50,142 +47,156 @@ lcount.toString(lostCount);
  * spent do you complete the game and have beaten the computer.
  * (later versions should include chances to win a life back e.g. 3 straight wins = 1 life)
  */
-let playLives=3,comLives=3;
+let playLives = 0, comLives = 3;
 
 /**
  * selecting a hand type
  */
 document.addEventListener("DOMContentLoaded", function(){
-    let hands = document.getElementsByTagName("button");
 
-    for(let button of hands){
-        button.addEventListener("click",function(){
-            
-            if(playLives > 0 && comLives > 0){}
-    
-           
-            if(this.getAttribute("data-type") ==="submit"){
-                if(playerSelect== 0){
-                    alert("Please pick a hand");
-                }
-                else if(playerSelect != 0){
-                     /**
-                      * this is were you start computer random selection
-                      */
+    if(playLives > 0 || comLives > 0){
+        let hands = document.getElementsByTagName("button");
+
+        for(let button of hands){
+            button.addEventListener("click",function(){
                 
-                   let random = Math.floor(Math.random()*5)+1;
-                   alert(`computer selected ${random}`);
-                   let icon=random;
-                   switch(icon){
-                       case 1 : document.getElementById('computersChoosen').innerHTML='<div id="rocky"></div>'; break;
-                       case 2 : document.getElementById('computersChoosen').innerHTML='<div id="hssss"></div>'; break;
-                       case 3 : document.getElementById('computersChoosen').innerHTML='<div id="vulcan"></div>'; break;
-                       case 4 :  document.getElementById('computersChoosen').innerHTML='<div id="wrap"></div>'; break;
-                       case 5 :  document.getElementById('computersChoosen').innerHTML='<div id="rocky"></div>'; break;
-                       default: alert("The computer crashed!"), clear();
-                   }
-                    switch(random){
-                        case 1 : computerSelect= rock; break;
-                        case 2 : computerSelect= lizard; break;
-                        case 3 : computerSelect= spock; break;
-                        case 4 : computerSelect= paper; break;
-                        case 5 : computerSelect= scissors; break;
+            
+        
+            
+                if(this.getAttribute("data-type") ==="submit"){
+                    if(playerSelect== 0){
+                        alert("Please pick a hand");
+                    }
+                    else if(playerSelect != 0){
+                        /**
+                         * this is were you start computer random selection
+                         */
+                    
+                    let random = Math.floor(Math.random()*5)+1;
+                    let icon=random;
+                    switch(icon){
+                        case 1 : document.getElementById('computersChoosen').innerHTML='<div id="rocky"></div>'; break;
+                        case 2 : document.getElementById('computersChoosen').innerHTML='<div id="hssss"></div>'; break;
+                        case 3 : document.getElementById('computersChoosen').innerHTML='<div id="vulcan"></div>'; break;
+                        case 4 :  document.getElementById('computersChoosen').innerHTML='<div id="wrap"></div>'; break;
+                        case 5 :  document.getElementById('computersChoosen').innerHTML='<div id="rocky"></div>'; break;
                         default: alert("The computer crashed!"), clear();
                     }
-                    alert(`computer selected ${computerSelect}`);
-                    console.log(computerSelect);
-                    alert("StartGame");
-                    startGame();
+                        switch(random){
+                            case 1 : computerSelect= rock; break;
+                            case 2 : computerSelect= lizard; break;
+                            case 3 : computerSelect= spock; break;
+                            case 4 : computerSelect= paper; break;
+                            case 5 : computerSelect= scissors; break;
+                            default: alert("The computer crashed!"), clear();
+                        }
+                        console.log(computerSelect);
+                        startGame();
+                    }
+                } 
+                else if(this.getAttribute("data-type") ==="rock"){
+                    clear();
+                    alert("you selected rock");
+                    console.log("you selected rock");
+                    rock=1,spock=2,paper=2;
+                    playerSelect=rock;
+                    document.getElementById('playersChoosen').innerHTML='<div id="rocky"></div>';
+                    
                 }
-            } 
-            else if(this.getAttribute("data-type") ==="rock"){
-                clear();
-                alert("you selected rock");
-                console.log("you selected rock");
-                rock=1,spock=2,paper=2;
-                playerSelect=rock;
-                document.getElementById('playersChoosen').innerHTML='<div id="rocky"></div>';
-                
-             }
-            else if(this.getAttribute("data-type") ==="lizard"){
-                clear();
-                alert("you selected lizard");
-                console.log("you selected lizard");
-                lizard=1,scissors=2,rock=2;
-                playerSelect=lizard;
-                document.getElementById('playersChoosen').innerHTML='<div id="hssss"></div>';
-             } 
-             else if(this.getAttribute("data-type") ==="spock"){
-                clear();
-                alert("you selected spock");
-                console.log("you selected spock");
-                spock=1,paper=2,lizard=2;
-                playerSelect=spock;
-                document.getElementById('playersChoosen').innerHTML='<div id="vulcan"></div>';
-             }
-             else if(this.getAttribute("data-type") ==="paper"){
-                alert("you selected paper");
-                console.log("you selected paper");
-                paper=1,scissors=2,lizard=2;
-                playerSelect=paper;
-                document.getElementById('playersChoosen').innerHTML='<div id="wrap"></div>';
-             }
-             else if(this.getAttribute("data-type") ==="scissors"){
-                alert("you selected scissor");
-                console.log("you selected scissors");
-                scissors=1,rock=2,spock=2;
-                playerSelect=scissors;
-                document.getElementById('playersChoosen').innerHTML='<div id="cutting"></div>';
-             }    
-            else{
-                console.log("wrong selection ");
-            }
-        });
+                else if(this.getAttribute("data-type") ==="lizard"){
+                    clear();
+                    alert("you selected lizard");
+                    console.log("you selected lizard");
+                    lizard=1,scissors=2,rock=2;
+                    playerSelect=lizard;
+                    document.getElementById('playersChoosen').innerHTML='<div id="hssss"></div>';
+                } 
+                else if(this.getAttribute("data-type") ==="spock"){
+                    clear();
+                    alert("you selected spock");
+                    console.log("you selected spock");
+                    spock=1,paper=2,lizard=2;
+                    playerSelect=spock;
+                    document.getElementById('playersChoosen').innerHTML='<div id="vulcan"></div>';
+                }
+                else if(this.getAttribute("data-type") ==="paper"){
+                    alert("you selected paper");
+                    console.log("you selected paper");
+                    paper=1,scissors=2,lizard=2;
+                    playerSelect=paper;
+                    document.getElementById('playersChoosen').innerHTML='<div id="wrap"></div>';
+                }
+                else if(this.getAttribute("data-type") ==="scissors"){
+                    alert("you selected scissor");
+                    console.log("you selected scissors");
+                    scissors=1,rock=2,spock=2;
+                    playerSelect=scissors;
+                    document.getElementById('playersChoosen').innerHTML='<div id="cutting"></div>';
+                }    
+                else{
+                    console.log("wrong selection ");
+                }
+            });
+        }
+    }
+    else{
+        alert("the is Game Over")
+        clear();
     }
 });
 
 function startGame(){
-   
-    if(playLives > 0 && comLives > 0){
-
-        if(playerSelect > computerSelect){
-            alert(`${playerSelect} > ${computerSelect}`);
-            alert("You win");
-            ++winCount;
-            document.getElementById('playersChoosen').style.border = 'solid 5px green';
-            document.getElementById('computersChoosen').style.border = 'solid 5px red';  
-            document.getElementById("winCount").innerText=winCount;
-            --comLives;
-            cLives();
-
+    gameStatus = document.getElementById("startButton").innerText;
+    alert(gameStatus)
+    if (gameStatus == "START"){
+        if(playLives == 0){
+            document.getElementById("status").innerText = "Game over";
+            document.getElementById("startButton").innerText = "Restart";
+            alert("You lost the game")
         }
-        else if(playerSelect < computerSelect){
-            alert(`${playerSelect} < ${computerSelect}`);
-            alert("You Lost");
-            ++lostCount;
-            document.getElementById('playersChoosen').style.border = 'solid 5px red';
-            document.getElementById('computersChoosen').style.border = 'solid 5px green'; 
-            document.getElementById("lostCount").innerText=lostCount;
-            --playLives;
-            document.getElementById('lostCount').style.fontSize = 20;
-            pLives();
+        else if (comLives == 0) {
+            document.getElementById("status").innerText = "Game over";
+            document.getElementById("startButton").innerText = "Restart";
+            alert("You beat the computer");
         }
         else{
-            alert(`${playerSelect} = ${computerSelect}`);
-            alert("Its a Draw");
+    /*
+            if(playerSelect > computerSelect){
+                document.getElementById("status").innerText = "You won";
+                ++winCount;
+                document.getElementById('playersChoosen').style.border = 'solid 5px green';
+                document.getElementById('computersChoosen').style.border = 'solid 5px red'; 
+                --comLives;
+                cLives();
+                document.getElementById("victory").innerText = winCount;
+                document.getElementById("victory").innerText.toString;
+            
+
+            }
+            else if(playerSelect < computerSelect){
+                document.getElementById("status").innerText = "You Lost";
+                ++lostCount;
+                document.getElementById('playersChoosen').style.border = 'solid 5px red';
+                document.getElementById('computersChoosen').style.border = 'solid 5px green'; 
+                --playLives;
+                pLives();
+                document.getElementById("defeat").innerText = lostCount;
+                document.getElementById("defeat").innerText.toString;
+            }
+            else{
+                document.getElementById("status").innerText = "Draw";
+                document.getElementById('playersChoosen').style.border = 'solid 5px gray';
+                document.getElementById('computersChoosen').style.border = 'solid 5px gray';
+            }  
+            */  
+        alert("game on")
         }
-        
-        
     }
-    else if(playLives=0){
-        alert("Game over");
+    else{
+        alert("restart initiating")
+    /* refresh page*/
+    window.location.reload();
     }
-    else {
-        alert("You beat the computer");
-    }
-    console.log(`player lives ${playLives} and computer lives ${comLives}`);
-
-
 }
  
 function pLives(){
